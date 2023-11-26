7be73bcb3a3046720a37b050ddb2b11bf6d3ac7d
fatihmobim <fatih@mobimyazilim.com>
2023-11-26 23:19:09 +0300

    Dashboard yapıldı.

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -5,15 +5,16 @@ const Input = ({
     name = "",
     type = "text",
     className = "",
+    InputclassName = "",
     isRequired = false,
     placeHolder = "",
     value = "",
     onChange = () => {}
 }) => {
   return (
-    <div className='w-1/2'>
+    <div className={`w-1/2 ${className}`}>
         <label for = {name}  className='block text-sm font-medium text-gray-800 ' > {label} </label>
-        <input className= {` bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${className}`} 
+        <input className= {` bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 ${InputclassName}`} 
         type={type} id= {name} placeholder={placeHolder} required={isRequired} value={value} onChange={onChange}
        />
 
